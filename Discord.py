135b37ba005b65148ceb8636a5662f197b59a025
RandomVapeUser <121972940+RandomVapeUser@users.noreply.github.com>
2023-11-24 20:51:19 +0000

    V1 Final Push

--- Discord.py
+++ Discord.py
@@ -1,4 +1,4 @@
-from discord_webhook import DiscordWebhook as dc
+from discord_webhook import DiscordWebhook, DiscordEmbed
 import sys
 import os
 import time
@@ -11,38 +11,69 @@ print("""
 
     """)
 
-X = input(" ")
+X = int(input(" "))
 
+file_name2 = " "
 user = os.environ.get("USERNAME")
 
-while True: 
+def file_name():
+    
+    global file_name2
 
-    if X == 1:
-        os.system("cls")
-        Y = input("Name: ")
-        time.sleep(2)
-        Z = input("File name: ")
-        break
-    elif X == 2:
-        print("Test")
-        time.sleep(2)
-        print("Exiting...")
-        time.sleep(2)
-        sys.exit()
-    else:
-        print("No such number!")
-        os.system("cls")
-        time.sleep(2)
-        continue
+    while True:
+        
+        try: 
+            if X == 1:
+                os.system("cls")
+                Y = input("Name: ")
+                time.sleep(2)
+                Z = input("File name: ")
+                file_name2 = Z
+                break
+            elif X == 2:
+                print("Test")
+                time.sleep(2)
+                print("Exiting...")
+                time.sleep(2)
+                sys.exit()
+            else:
+                print("No such number!")
+                os.system("cls")
+                time.sleep(2)
+                os.system("cls")
+                continue
+        except SyntaxError:
+            print("Nuh uh print numbers not words you idiot")
+            os.system("cls")
+            continue
 
-webhook = dc(url = "https://discord.com/api/webhooks/1172913800006094888/2wYTsKqbvPfiDE4mCIxous-hDZcJzYKhr6JMIitQuPWvZZSpkYN7jufZpiUlu9eN6jKN")
 
-embed = DiscordEmbed(title = "Discord File", description = "Success")
+
+webhook = DiscordWebhook(url = "https://discord.com/api/webhooks/1172913800006094888/2wYTsKqbvPfiDE4mCIxous-hDZcJzYKhr6JMIitQuPWvZZSpkYN7jufZpiUlu9eN6jKN")
+
+embed = DiscordEmbed(title="Test", description="New File!", color="03b2f8")
 webhook.add_embed(embed)
 
-with open(f"C:\Users\{user}\Desktop\{Z}") as file:
-    webhook.add_file(file=file.read(), filename="Test File")
+def send_file():
 
-webhook.execute()
-print("File Successfully sent!")
-print("Goodbye!")
+    global Z
+    global user
+
+    with open(fr"C:\Users\{user}\Downloads\{file_name2}", 'rb') as file:
+        content = file.read()
+        webhook.add_file(file=content, filename="Test File")
+        webhook.execute()
+        print("File Successfully sent!")
+        print("Goodbye!")
+        time.sleep(2)
+        sys.exit()
+
+while True:
+    try:
+        file_name()
+        send_file()
+    except FileNotFoundError:
+        print("File not found, please try again")
+        time.sleep(2)
+        os.system("cls")
+        continue
